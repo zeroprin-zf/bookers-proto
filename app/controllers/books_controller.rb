5e58dc3a66e7b5a89036415729e797a181c7615a
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2024-01-05 01:38:34 +0000

    機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,39 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all#すべてのdbの全てのレコードを取得する
+    @book = Book.new
+  end
+
+  def create
+    #データを受け取り新規登録するためのインスタンスを作成。コントローラでしか使わないためローカル変数
+    book = Book.new(book_params)#モデルのインスタンスを作成
+    #データをデータベースに保存するためのsaveメソッド
+    book.save#モデルインスタンスをデータベースに保存するDBに実際に保存される
+    redirect_to book_path(book.id) #仮
   end
 
   def show
+    @book = Book.find(params[:id]) #idが適した数字のレコードを取得
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+  end
+
+  private #private コントローラの中でしか呼び出せない領域
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  #params formから送られてくるデータはparamsの中にある
+  #require 送られてきたデータの中からモデル名を指定し、データを送り込む
+  #permit requireで絞り込んだデータの中から、保存を許可するカラムを指定する
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
+  root to: 'homes#top'
+  get 'books' => 'books#index' #get リンクを指定 コントローラのなんのアクションかを#の後に指定
+  post 'books' => 'books#create'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'book/:id' => 'books#update', as: 'update_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
